210cabfa6550d29cb5512fbefc0357af13444f8e
Patryk Niczke <kyrtap1301@gmail.com>
2024-03-21 18:31:59 +0100

    I've added code to calculate earth velocity around the Sun

--- src/earth.py
+++ src/earth.py
@@ -2,7 +2,12 @@
 import spiceypy
 
 #import datetime to work with date data
-import datetime 
+import datetime
+
+#import math to use math functions
+import math
+
+AU_TO_KM = 149_597_871
 
 #spicepy needs a kernels loaded to work properly
 spiceypy.furnsh('./kernels/lsk/naif0012.tls.txt')
@@ -12,7 +17,7 @@ spiceypy.furnsh('./kernels/spk/de440s.bsp')
 today = datetime.datetime.today()
 
 #convert today to string and set midnight
-today = today.strftime('%Y-%M-%dT00:00:00')
+today = today.strftime('%Y-%m-%dT00:00:00')
 
 #convert UTC to ET
 et_today = spiceypy.utc2et(today)
@@ -28,5 +33,17 @@ et_today = spiceypy.utc2et(today)
 earth_state_vector, earth_sun_light_time = spiceypy.spkgeo(targ=399,
     et = et_today, ref='ECLIPJ2000', obs = 10)
 
-#print(earth_state_vector)
-print(earth_sun_light_time)
+#Calculate earth - sun distance (km)
+earth_sun_distace = math.sqrt(earth_state_vector[0]**2+earth_state_vector[1]**2+
+                              earth_state_vector[2]**2)
+
+#Calculate the orbital speed of the Earth around the Sun (km/s)
+earth_sun_speed = math.sqrt(earth_state_vector[3]**2+earth_state_vector[4]**2+
+                               earth_state_vector[5]**2)
+
+#Convert a distance to AU
+au_earth_sun_distance = earth_sun_distace/AU_TO_KM
+
+print(f"Earth location in km in relation to Sun for {today}: {earth_state_vector}")
+print(f"Earth distace from Sum (in AU) equals for {today}: {au_earth_sun_distance}")
+print(f"The Earth orbital speed around the Sun equals for: {earth_sun_speed}")
