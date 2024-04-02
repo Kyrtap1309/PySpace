a85de4273c41ee7d757e208fe833dee966a0c4ab
Patryk Niczke <kyrtap1301@gmail.com>
2024-04-02 12:01:36 +0200

    Make a object oriented first kepler file

--- src/first_kepler.py
+++ src/first_kepler.py
@@ -1,4 +1,4 @@
-# TO DO make it object oriented
+
 # TO DO make plot animated
 
 import datetime 
@@ -8,77 +8,82 @@ import matplotlib.pyplot as plt
 
 from kernel_manager import kernels_load
 
-#spicepy needs a kernels loaded to work properly
-kernels_path = ['./kernels/lsk/naif0012.tls.txt',
-                './kernels/pck/pck00010.tpc.txt',
-                './kernels/spk/de440s.bsp']
+class FirstKepler:
+    def __init__(self):
+        #spicepy needs a kernels loaded to work properly
+        self.kernels_path = ['./kernels/lsk/naif0012.tls.txt',
+                        './kernels/pck/pck00010.tpc.txt',
+                        './kernels/spk/de440s.bsp']
 
-kernels_load(kernels_path)
+        kernels_load(self.kernels_path)
 
-#Initialization of UTC time
-init_utc_time = datetime.datetime(year=2000, month=1, day=1,
-                                  hour=0, minute=0, second=0)
+        #Initialization of UTC time
+        self.init_utc_time = datetime.datetime(year=2000, month=1, day=1,
+                                        hour=0, minute=0, second=0)
 
-#how many days between end of calculations and initialization time
-days_diff = 40000
-end_utc_time = init_utc_time + datetime.timedelta(days=days_diff)
+        #how many days between end of calculations and initialization time
+        self.days_diff = 40000
+        self.end_utc_time = self.init_utc_time + datetime.timedelta(days=self.days_diff)
 
-#Convert dates to strings
-init_utc_time_str = init_utc_time.strftime('%Y-%m-%dT%H:%M:%S')
-end_utc_time_str = end_utc_time.strftime('%Y-%m-%dT%H:%M:%S')
+        #Convert dates to strings
+        self.init_utc_time_str = self.init_utc_time.strftime('%Y-%m-%dT%H:%M:%S')
+        self.end_utc_time_str = self.end_utc_time.strftime('%Y-%m-%dT%H:%M:%S')
 
-#Print the starting and end times
-print(f"Start day: {init_utc_time_str}")
-print(f"End day: {end_utc_time_str}")
+        #Print the starting and end times
+        print(f"Start day: {self.init_utc_time_str}")
+        print(f"End day: {self.end_utc_time_str}")
 
-#Ephemeris time 
-init_et_time = spiceypy.utc2et(init_utc_time_str)
-end_et_time = spiceypy.utc2et(end_utc_time_str)
+        #Ephemeris time 
+        self.init_et_time = spiceypy.utc2et(self.init_utc_time_str)
+        self.end_et_time = spiceypy.utc2et(self.end_utc_time_str)
 
-#create numpy array with one day interval between start and end day
-time_array = np.linspace(init_et_time, end_et_time, days_diff) 
+        #create numpy array with one day interval between start and end day
+        self.time_array = np.linspace(self.init_et_time, self.end_et_time, self.days_diff) 
 
-#Array with all positions of solar system barycentre
-solar_system_barycentre_pos = []
+        #Array with all positions of solar system barycentre
+        self.solar_system_barycentre_pos = []
 
-for time in time_array:
-    _position, _ =spiceypy.spkgps(targ=0, et=time, ref ='ECLIPJ2000',
-                                  obs = 10)
-    solar_system_barycentre_pos.append(_position)
+        for time in self.time_array:
+            _position, _ =spiceypy.spkgps(targ=0, et=time, ref ='ECLIPJ2000',
+                                        obs = 10)
+            self.solar_system_barycentre_pos.append(_position)
 
-#convert to numpy array
-solar_system_barycentre_pos = np.array(solar_system_barycentre_pos)
+        #convert to numpy array
+        self.solar_system_barycentre_pos = np.array(self.solar_system_barycentre_pos)
 
-#import sun radius
-_, sun_radius_arr = spiceypy.bodvcd(bodyid=10, item='RADII', maxn=3)
-sun_radius = sun_radius_arr[0]
+        #import sun radius
+        _, sun_radius_arr = spiceypy.bodvcd(bodyid=10, item='RADII', maxn=3)
+        self.sun_radius = sun_radius_arr[0]
 
-#Scalled solar system barycentre position (in Sun radii)
-solar_system_barycentre_pos_scalled = solar_system_barycentre_pos/sun_radius
+        #Scalled solar system barycentre position (in Sun radii)
+        self.solar_system_barycentre_pos_scalled = self.solar_system_barycentre_pos/self.sun_radius
 
-#Plotting trajectory of solar system barycentre (only needed x and y coordinates)
-solar_system_barycentre_pos_scalled_plane = solar_system_barycentre_pos_scalled[:, 0:2]
+        #Plotting trajectory of solar system barycentre (only needed x and y coordinates)
+        self.solar_system_barycentre_pos_scalled_plane = self.solar_system_barycentre_pos_scalled[:, 0:2]
 
-plt.style.use('dark_background')
+    def trajectory(self):
+        plt.style.use('dark_background')
 
-fig, ax = plt.subplots(figsize=(15, 10))
+        fig, ax = plt.subplots(figsize=(15, 10))
 
-#Begin with plotting the sun
-sun_plot = plt.Circle((0.0, 0.0), 1.0, color='yellow')
-ax.add_artist(sun_plot)
-ax.plot(solar_system_barycentre_pos_scalled_plane[:, 0], 
-        solar_system_barycentre_pos_scalled_plane[:, 1],
-        color='green')
-ax.grid(True, linewidth=0.5, linestyle='dashed', alpha=0.7)
-ax.set_aspect('equal')
-ax.set_xlim(-5, 5)
-ax.set_ylim(-5, 5)
-
-
-ax.set_xlabel('x in sun radius')
-ax.set_ylabel('y in sun radius')
-plt.title('Trajectory of solar system barycentrum')
+        #Begin with plotting the sun
+        sun_plot = plt.Circle((0.0, 0.0), 1.0, color='yellow')
+        ax.add_artist(sun_plot)
+        ax.plot(self.solar_system_barycentre_pos_scalled_plane[:, 0], 
+                self.solar_system_barycentre_pos_scalled_plane[:, 1],
+                color='green')
+        ax.grid(True, linewidth=0.5, linestyle='dashed', alpha=0.7)
+        ax.set_aspect('equal')
+        ax.set_xlim(-5, 5)
+        ax.set_ylim(-5, 5)
 
 
-plt.show()
+        ax.set_xlabel('x in sun radius')
+        ax.set_ylabel('y in sun radius')
+        plt.title('Trajectory of solar system barycentrum')
 
+        plt.show()
+
+if __name__ == "__main__":
+    first_kepler = FirstKepler()
+    first_kepler.trajectory()
